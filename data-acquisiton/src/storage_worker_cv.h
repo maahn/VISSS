aafd49ebf43a98068eae507abc772d68e9b0de5d
Maximilian Maahn <maximilian.maahn@colorado.edu>
2019-07-29 04:51:36 +0000

    tiff -> jpg

--- data-acquisiton/src/storage_worker_cv.h
+++ data-acquisiton/src/storage_worker_cv.h
@@ -212,7 +212,7 @@ void storage_worker_cv::run()
                     close_files();
                     open_files();
 
-                    cv::imwrite(filename_+".tiff", image.MatImage );
+                    cv::imwrite(filename_+".jpg", image.MatImage );
                     frame_count_new_file = frame_count;
                     std::cout << "STATUS | " << get_timestamp() << " | Written "<< filename_+".jpg"<< std::endl;
 
