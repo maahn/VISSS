348daa0104edef896fb4e86508c2c4a88a0b0a58
VISSS01 <VISSS01@igmk>
2023-01-02 11:10:15 +0000

    added status report script

---
+++ scripts/statusReport.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "<html><pre>"
+
+echo "********************************************************************************"
+echo uptime
+uptime
+
+echo "********************************************************************************"
+echo ps -eo pcpu,pmem,pid,user,args | sort -r -k1 | head
+ps -eo pcpu,pmem,pid,user,args | sort -r -k1 | head
+
+echo "********************************************************************************"
+echo df -h / /data
+df -h / /data
+
+echo "</pre></html>"
+
